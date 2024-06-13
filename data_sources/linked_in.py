d9af86f0dc7eee2cf7448971dac800383e3fef19
luc-pimentel <muleste@proton.me>
2024-06-13 15:24:38 -0300

    Add pagination for job searches to LinkedIn API

--- data_sources/linked_in.py
+++ data_sources/linked_in.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 from ._base import BaseRestfulAPI
 from ..data_lake.logger import log_io_to_json
 import warnings
-
+import time
 
 
 
@@ -51,15 +51,9 @@ class LinkedInAPI(BaseRestfulAPI):
         if min_salary and str(min_salary) not in min_salary_dict.keys():
             raise ValueError(f"Invalid minimum salary {min_salary}. Choose from '40000', '60000', '80000', '100000', '120000' in order to fit with the LinkedIn search filters.")
 
-
-
-        endpoint = 'jobs-guest/jobs/api/seeMoreJobPostings/search'
-
         params = {"keywords": search_query,
                   }
 
-
-        #endpoint += search_query.replace(' ', '-') + '-jobs?'
         params['location'] = location
         params['f_TPR'] = f'r{days_ago*86400}' if days_ago else None
         params['f_JT'] = job_type[0].upper() if job_type else None
@@ -68,6 +62,7 @@ class LinkedInAPI(BaseRestfulAPI):
         params['f_SB2'] = min_salary_dict[str(min_salary)] if min_salary else None
         params['start'] = start if start else 0
 
+        # Remove None values from the dict
         params = {k: v for k, v in params.items() if v is not None}
         return params
 
@@ -88,29 +83,38 @@ class LinkedInAPI(BaseRestfulAPI):
 
 
     @log_io_to_json
-    def get_job_postings_data(self, search_query: str, **kwargs):
-        soup = self.search(search_query, **kwargs)
-
-        results_list = soup.find_all('li')
-
+    def get_job_postings_data(self, search_query: str, n_listings=10, **kwargs):
         job_listings = []
-        for result in results_list:
-            
-            title = result.find('h3', class_='base-search-card__title')
-            company = result.find('h4', class_='base-search-card__subtitle')
-            location = result.find('span', class_='job-search-card__location')
-            time_element = result.find('time')
-            link = result.find('a', class_='base-card__full-link')
+        start = 0
 
+        while len(job_listings) < n_listings:
+            soup = self.search(search_query, start=start, **kwargs)
+            results_list = soup.find_all('li')
 
-            result_dict = {'title': title.text.strip() if title else None,
-                        'company': company.text.strip() if company else None,
-                        'location': location.text.strip() if location else None,
-                        'time': time_element.get('datetime') if time_element else None,
-                        'link': link.get('href') if link else None}
-            
-            job_listings.append(result_dict)
+            if not results_list:
+                break  # Exit if no more results are found
 
+            for result in results_list:
+                if len(job_listings) >= n_listings:
+                    break  # Stop if we have enough listings
 
+                title = result.find('h3', class_='base-search-card__title')
+                company = result.find('h4', class_='base-search-card__subtitle')
+                location = result.find('span', class_='job-search-card__location')
+                time_element = result.find('time')
+                link = result.find('a', class_='base-card__full-link')
+
+                result_dict = {
+                    'title': title.text.strip() if title else None,
+                    'company': company.text.strip() if company else None,
+                    'location': location.text.strip() if location else None,
+                    'time': time_element.get('datetime') if time_element else None,
+                    'link': link.get('href') if link else None
+                }
+
+                job_listings.append(result_dict)
+
+            start += len(results_list)
+            time.sleep(10)  # Move to the next page
 
         return job_listings
